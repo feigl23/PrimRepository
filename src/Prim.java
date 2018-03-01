3e03681b3600cf7c776e98a0f6b2bd13a7ddbce5
feigl23@gmail.com <feigl@iit.uni-miskolc.hu>
2018-03-01 10:49:40 +0100

    initial commit

---
+++ src/Prim.java
@@ -0,0 +1,36 @@
+
+public class Prim {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		    
+		for(int i =1; i <=100;)
+		
+		
+
+		   }
+	public static boolean IsPrim (int num)
+	{
+	 if(num <2) return false; 
+	 if (num ==2;)return true;
+	 if(num%2 ==0) return false;
+	 
+	 for(int i =0; i *i <=num; i +=2)
+	 {
+		 System.out.println(i);
+		 if(num % i ==0) return false;
+	 }
+	 return true;
+	 }
+	
+		
+	}
+
+	}
+	
+  
+
